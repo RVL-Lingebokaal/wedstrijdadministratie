b10d3ce48c30a6bce3f857414dcaea04d7f52778
Tessa Vermaas <tessa.vermaas@gmail.com>
2023-05-11 19:35:20 +0200

    Added a basic design to the application

---
+++ src/components/molecules/page/Page.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  && {
+    padding: 20px 80px;
+  }
+`;
+
+export const Container = styled.div`
+  && {
+    display: flex;
+    flex-direction: column;
+  }
+`;
