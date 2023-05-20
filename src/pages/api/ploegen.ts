ee6f9ed957187c9524505d81cbd75ff7d286c795
Tom Veltmeijer <VeltmeijerT@wigo4it.nl>
2023-05-20 14:46:32 +0200

    Add Firebase database connection
    - Firebase is only accessed server-side via an API route

---
+++ src/pages/api/ploegen.ts
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { collection, getDocs } from 'firebase/firestore';
+import firestore from '../../firebase/firebase';
+import type { NextApiRequest, NextApiResponse } from "next";
+
+class Ploeg {
+  naam: string = '';
+
+  constructor(naam: string) {
+    this.naam = naam;
+  }
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Ploeg[]>
+) {
+  const dbInstance = collection(firestore, 'ploeg');
+  const data = await getDocs(dbInstance);
+  const ploegen = data.docs.map((doc) => new Ploeg(doc.data().naam));
+  res.status(200).json(ploegen);
+}
--- src/pages/data/index.tsx
+++ src/pages/data/index.tsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from 'react';
+
+class Ploeg {
+  naam: string = '';
+
+  constructor(naam: string) {
+    this.naam = naam;
+  }
+}
+
+let shouldLoad = true;
+
 export default function Data() {
+  const [ploegen, setPloegen] = useState<Ploeg[]>([]);
+
+  useEffect(() => {
+    if (shouldLoad) {
+      shouldLoad = false;
+      fetch('/api/ploegen')
+        .then((response) => {
+          return response.json();
+        })
+        .then((ploegen : Ploeg[]) => {
+          setPloegen(ploegen);
+        })
+    }
+  });
+
   return (
     <div>
       <h1>Data</h1>
+
+      <h2>Ploegen</h2>
+      {ploegen.map(ploeg => (
+        <div key={ploeg.naam}>
+          <p>{ploeg.naam}</p>
+        </div>
+      ))}
     </div>
   );
 }
