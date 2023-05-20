5b28e2951ed278bb93d5f7c88040d11e74a7ecc5
Tom Veltmeijer <VeltmeijerT@wigo4it.nl>
2023-05-20 14:48:31 +0200

    Remove comment

--- src/pages/api/ploegen.ts
+++ src/pages/api/ploegen.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { collection, getDocs } from 'firebase/firestore';
 import firestore from '../../firebase/firebase';
 import type { NextApiRequest, NextApiResponse } from "next";
