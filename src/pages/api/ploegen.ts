6952198cabb1be1a7223fc76f5f0af4cba8bb17f
Tessa Vermaas <tessa.vermaas@gmail.com>
2023-07-30 15:17:40 +0200

    Add a new interface for Ploeg

--- src/pages/api/ploegen.ts
+++ src/pages/api/ploegen.ts
@@ -1,21 +1,14 @@
-import { collection, getDocs } from 'firebase/firestore';
-import firestore from '../../firebase/firebase';
+import { collection, getDocs } from "firebase/firestore";
+import firestore from "../../firebase/firebase";
 import type { NextApiRequest, NextApiResponse } from "next";
-
-class Ploeg {
-  naam: string = '';
-
-  constructor(naam: string) {
-    this.naam = naam;
-  }
-}
+import { Ploeg } from "../../models/ploeg";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Ploeg[]>
 ) {
-  const dbInstance = collection(firestore, 'ploeg');
+  const dbInstance = collection(firestore, "ploeg");
   const data = await getDocs(dbInstance);
-  const ploegen = data.docs.map((doc) => new Ploeg(doc.data().naam));
+  const ploegen: Ploeg[] = data.docs.map((doc) => ({ name: doc.data().name }));
   res.status(200).json(ploegen);
 }
