2dceb17b60103de1788a52ca9a38cf7b8a3a9a28
Tessa Vermaas <tessa.vermaas@gmail.com>
2023-09-10 21:42:22 +0200

    Added typing to forward ref

--- src/components/atoms/input/input.tsx
+++ src/components/atoms/input/input.tsx
@@ -1,21 +1,26 @@
-import { forwardRef } from "react";
+import { ForwardedRef, forwardRef } from "react";
 
 interface InputProps {
   onChange: () => void;
   label?: string;
 }
 
-export const Input = forwardRef(({ label, ...props }: InputProps, ref) => {
-  return (
-    <div className="mt-2">
-      <label className="font-bold">{label}</label>
-      <input
-        className="border-gray-400 border rounded-lg px-2 py-1.5 w-full"
-        type="text"
-        ref={ref}
-        {...props}
-      />
-    </div>
-  );
-});
+export const Input = forwardRef(
+  (
+    { label, ...props }: InputProps,
+    ref: ForwardedRef<HTMLInputElement | null>
+  ) => {
+    return (
+      <div className="mt-2">
+        <label className="font-bold">{label}</label>
+        <input
+          className="border-gray-400 border rounded-lg px-2 py-1.5 w-full"
+          type="text"
+          ref={ref}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
 Input.displayName = "Input";
