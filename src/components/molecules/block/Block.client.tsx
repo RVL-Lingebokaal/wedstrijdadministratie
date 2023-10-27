223ad48263666d2e8d533ddc406850c53ef732ca
Tessa Vermaas <tessa.vermaas@gmail.com>
2023-10-27 14:15:18 +0200

    Add loading check to front page

--- src/components/molecules/block/Block.client.tsx
+++ src/components/molecules/block/Block.client.tsx
@@ -6,26 +6,16 @@ interface BlockProps {
   bottom?: string;
   children: ReactNode;
   variant: "small" | "large";
-  isLoading?: boolean;
 }
 
-export function Block({
-  title,
-  bottom,
-  children,
-  variant,
-  isLoading,
-}: BlockProps) {
+export function Block({ title, bottom, children, variant }: BlockProps) {
   return (
     <div
       className={`place-items-center flex flex-col border-4 border-secondary-500 bg-white flex-1 ${
         variant === "small" ? "px-7 py-5" : "p-12"
       }`}
     >
-      {title && !isLoading && (
-        <h4 className="text-4xl font-extrabold">{title}</h4>
-      )}
-      {isLoading && <LoadingSpinner />}
+      {title && <h4 className="text-4xl font-extrabold">{title}</h4>}
       {children}
       {bottom && <p className="text-3xl uppercase">{bottom}</p>}
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,32 +44,30 @@ export default function Home() {
     <div>
       <h1 className="text-white text-7xl font-bold">RVL Lingebokaal</h1>
       <h2 className="text-white text-6xl">Wedstrijdadministratie</h2>
-      <div className="flex flex-row gap-x-4 mt-48">
-        {elements.map(({ title, bottom, icon }) => (
-          <Block
-            title={title}
-            bottom={bottom}
-            key={title}
-            variant="small"
-            isLoading={isLoading}
-          >
-            <Image src={icon} alt={title} className="py-2" />
-          </Block>
-        ))}
-      </div>
-      <div className="mt-12">
-        <Block variant="large">
-          <p className="text-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </Block>
-      </div>
+      {!isLoading && (
+        <>
+          <div className="flex flex-row gap-x-4 mt-48">
+            {elements.map(({ title, bottom, icon }) => (
+              <Block title={title} bottom={bottom} key={title} variant="small">
+                <Image src={icon} alt={title} className="py-2" />
+              </Block>
+            ))}
+          </div>
+          <div className="mt-12">
+            <Block variant="large">
+              <p className="text-xl">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </Block>
+          </div>
+        </>
+      )}
     </div>
   );
 }
